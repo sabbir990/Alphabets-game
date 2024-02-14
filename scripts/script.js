b2a6f50cbbf805be7fca566b48471a822fb65128
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-02-14 14:17:57 +0000

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const homeStartBtn = document.getElementById('home-start-btn');
+const playgroundSection = document.getElementById('playground-section');
+const homeSection = document.getElementById('home-section')
+
+
+homeStartBtn.addEventListener('click', function () {
+    homeSection.classList.add('hidden');
+    playgroundSection.classList.remove('hidden');
+    const displayAlpha = document.getElementById('alphabet');
+    displayAlpha.innerText = getRandomAlphabet();
+
+    const keyId = getRandomAlphabet();
+
+    const key = document.getElementById(keyId);
+    key.classList.add('bg-red-600', 'text-white')
+})
+
+function getRandomAlphabet() {
+    const allAlphabets = 'abcdefghijklmnopqrstuvwxyz';
+    const alphaArray = allAlphabets.split('');
+
+    const randomNumber = Math.round(Math.random() * 25);
+    return alphaArray[randomNumber];
+}
